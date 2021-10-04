8c1adc93a3400d873f14404f880372786c513213
bang9dev <gusrn1423@naver.com>
2021-10-04 18:12:33 +0900

    chore: added some setup information

--- src/libs/util/util.environment.ts
+++ src/libs/util/util.environment.ts
@@ -5,6 +5,12 @@ export const ENV_LIST = <const>["API_KEY_CAT", "API_URL_CAT"];
 export function checkEnvImplementation() {
     ENV_LIST.forEach(key => {
         // @ts-ignore
-        if (!environment[key]) throw new Error("Environment is not implemented - " + key);
+        if (!environment[key]) {
+            console.warn(
+                `Please run 'yarn env:dev' script for implement environment variables
+                For more information, visit README.md`
+            );
+            throw new Error("Environment is not implemented - " + key);
+        }
     });
 }
